673dba9f7a94d8d152235f2fa25de223c5ed474b
kokeshii <darthmincho@gmail.com>
2015-01-06 18:27:00 -0600

    JSON schema validation

--- remotes/models.py
+++ remotes/models.py
@@ -1,8 +1,20 @@
 # -*- coding: utf8 -*-
+import os
+import json
+
 from django.db import models
 from django_extensions.db.models import TimeStampedModel
+from django.core.exceptions import ValidationError
+
+import jsonschema
+from jsonschema import Draft4Validator
 
 from users import models as users
+
+def path_to_schema():
+	module_dir = os.path.dirname(__file__)
+	file_path = os.path.join(module_dir, "schema.json")
+	return file_path
 
 class Remote(TimeStampedModel):
 	name = models.CharField(max_length=100)
@@ -11,6 +23,23 @@ class Remote(TimeStampedModel):
 	configuration = models.TextField()
 
 	users = models.ManyToManyField(users.User)
+
+
+	def clean(self):
+
+		#Load the JSON schema
+		schema_file = open(path_to_schema())
+		schema = json.load(schema_file)
+
+		validator = Draft4Validator(schema)
+		configuration_json = json.loads(self.configuration)
+
+		try:
+			validator.validate(configuration_json)
+		except jsonschema.exceptions.ValidationError as e:
+			raise ValidationError(e.message)
+
+		return super(Remote, self).clean()
 
 	def __unicode__(self):
 		return self.name
