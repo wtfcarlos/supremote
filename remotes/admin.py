28bf9e5152d42f37adc813aca12027b8184e0150
kokeshii <darthmincho@gmail.com>
2015-01-27 21:21:13 -0600

    Add socket origin admin

--- remotes/admin.py
+++ remotes/admin.py
@@ -5,9 +5,14 @@ from . import models
 class RemoteAdmin(admin.ModelAdmin):
 	model = models.Remote
 
+class SocketOriginAdmin(admin.ModelAdmin):
+	model = models.SocketOrigin
+	list_display = ['domain', 'remote']
+
 class ActionThrottleAdmin(admin.ModelAdmin):
 	model = models.ActionThrottle
 	list_display = ['remote', 'key', 'status_code', 'created']
 
 admin.site.register(models.Remote, RemoteAdmin)
 admin.site.register(models.ActionThrottle, ActionThrottleAdmin)
+admin.site.register(models.SocketOrigin, SocketOriginAdmin)
